0681d63e0d4d18eb204ff89e5a420f08aedc17a1
Panagiotis Panagiotakopoulos <ppanagiotisilv@gmail.com>
2021-02-16 11:57:09 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,4 +2,6 @@ import 'w3-css/4/w3pro.css';
 import "../css/index.scss";
 import "https://kit.fontawesome.com/a72b9e7ca4.js";
 import {lastprojectsTemplate} from "./projectsDatabase";
+console.log(import.meta.env.WEATHER_KEY);
+console.log(import.meta.env.SNOWPACK_PUBLIC_KEY);
 lastprojectsTemplate();
