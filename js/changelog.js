73d6b2f3f961e4e263457bdf3b5eb26d560d467e
PanuGr <ppanagiotisilv@hotmail.com>
2021-04-20 10:33:13 +0300

    changelog

--- js/changelog.js
+++ js/changelog.js
@@ -15,16 +15,17 @@ const changelog = {
                     value: ' Removed Lodash.js library. Arrays and functions were refactored using native javascript'
                 },
                 { value: "Calculators spread into different files were brought together in one javascript file" },
-                { value: "Named functions converted into variables" }]
+                { value: "Named functions converted into variables" },
+                { value: "I wrote my first test(TDD) with mocha.js for couple of Mr.Funny's functions. Running the test I was surprised to see that my functions could actually be reformed in a better way!" }
+            ]
         },
         {
             date: '23.03.2021',
             updates: [
-                { value: "I wrote my first test(TDD) with mocha.js for couple of <a href='https://github.com/PanuGr/mrfunny' title='open in github' rel='noreferrer' target='_blank'>Mr Funny's functions</a> <i class='w3-small fas fa-external-link-square-alt'></i>. Running the test I was surprised to see that my functions could actually be reformed in a better way!" },
                 {
                     value: "Lodash.js library was removed from the <a href='https://github.com/PanuGr/javascriptGame' title='open in github' rel='noreferrer' target='_blank'>Rock-paper project</a> <i class='w3-small fas fa-external-link-square-alt'></i>, a website where you can play the game"
                 },
-                { value:  "For-loops were removed in favor of the shorter and more readable <code>for...of</code> loop" }]
+                { value: "For-loops were removed in favor of the shorter and more readable <code>for...of</code> loop" }]
         },
         {
             date: '24.03.2021',
@@ -34,15 +35,16 @@ const changelog = {
         {
             date: '08.04.2021',
             updates: [
-                { value: " I use the w3.CSS framework from npm and I noticed that npm serves the old version. Now, I am serving the newer version"},
-                { value: " All handlebars logic is exported as functions instead of importing the whole javascript file"},
-                { value: " I am making a 2nd html file, where visitors can see all the projects at once"}]
-        }/* ,
-        {
-            date: '24.03.2021',
-            updates: [
-                { value: " I added a TDD testing using the cypress.io framework on my <a href='https://github.com/PanuGr/rhyme' title='open in github' rel='noreferrer' target='_blank'>Poem-writer</a> <i class='w3-small fas fa-external-link-square-alt'></i> application" }]
+                { value: " I use the w3.CSS framework from npm and I noticed that npm serves the old version. Now, I am serving the newer version" },
+                { value: " All handlebars logic is exported as functions instead of importing the whole javascript file" }
+            ]
         },
+        {
+            date: '20.04.2021',
+            updates: [
+                { value: " Fixed the link to my <a href='https://getforecast.netlify.app' rel='noreferrer' target='_blank'>weather</a> <i class='w3-small fas fa-external-link-square-alt'></i> application" },
+                { value: "Deprecated @import statements in my Sass files were replaced by @use and @forward " }]
+        }/* ,
         {
             date: '24.03.2021',
             updates: [
