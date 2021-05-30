ffe859002654ae79895d49ceb1c2174a68f94d14
PanuGr <ppanagiotisilv@hotmail.com>
2021-05-30 14:56:46 +0300

    no w3.css

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
-import '../scss/w3.css';
-import "../scss/style.scss";
+//import "../scss/style.scss";
 import "https://kit.fontawesome.com/a72b9e7ca4.js";
 import { showProjects } from './projectsDatabase';
 import showCourses from './courses';
@@ -9,14 +8,16 @@ import { w3_open, w3_close } from './sidebarFunction';
 import scrollToLast from './scrollToLast';
 
 //SIDEBAR
-document.getElementById('myOverlay').addEventListener('click', w3_close)
+
+/* document.getElementById('myOverlay').addEventListener('click', w3_close)
 document.querySelector('#mySidebar > section > a').addEventListener('click', w3_close)
 document.querySelector('#mySidebar > div.w3-bar-block > a:nth-child(1)').addEventListener('click', w3_close)
 document.querySelector('#mySidebar > div.w3-bar-block > a:nth-child(2)').addEventListener('click', w3_close)
 document.querySelector('#mySidebar > div.w3-bar-block > a:nth-child(3)').addEventListener('click', w3_close)
 document.querySelector('#mySidebar > div.w3-bar-block > a:nth-child(4)').addEventListener('click', w3_close)
 document.querySelector('#mySidebar > div.w3-bar-block > a:nth-child(4)').addEventListener('click', scrollToLast)
-document.querySelector('header>span').addEventListener('click', w3_open)
+document.querySelector('header>span').addEventListener('click', w3_open) */
+
 //SHOW 2 RANDOM PROJECTS
 showProjects()
 //BUTTON UP
@@ -25,3 +26,4 @@ window.addEventListener('scroll', scrollFunction)
 //SCRIPT OF COMPLETED COURSES AND CHANGELOG
 showCourses()
 showChangelog()
+
