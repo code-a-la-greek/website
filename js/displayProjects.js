1e643234dd0faf82ea506f6158394aff32c2d8f9
PanuGr <ppanagiotisilv@hotmail.com>
2021-08-06 14:37:10 +0300

    hard-code of latest project on index.html

--- js/displayProjects.js
+++ js/displayProjects.js
@@ -1,15 +1,4 @@
-//Displaying the FIRST project
-const latestProject = {
-  title: 'Project with a two- column layout',
-  info: 'The layout is made with grid, with a grid-template-rows: auto 1fr auto;. The main content is divided in two columns automatically when the screen size is around 600px, using grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));',
-  website: 'https://panugr.github.io/landing-page/',
-  github: 'https://github.com/PanuGr/landing-page',
-  image: 'https://ik.imagekit.io/panugr/ProjectsDatabase/huddle-landing-page.jpg'
-}
-
 //DISPLAY THE PROJECTS
-showProjects('.flexbox',latestProject);
-
 function showProjects(element, number) {
   document.querySelector(`${element} h3`).innerHTML = number.title;
   document.querySelector(`${element} p`).innerHTML = number.info;
