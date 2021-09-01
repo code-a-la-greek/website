d020d8a7d3f605ccca34550f874d84e91fff5703
PanuGr <ppanagiotisilv@hotmail.com>
2021-09-01 16:22:24 +0300

    added res.ok. if not dont show project-card

--- js/projectsAPI.js
+++ js/projectsAPI.js
@@ -1,10 +1,18 @@
 export const getData = async () => {
     try {
         const res = await fetch("https://v1.nocodeapi.com/panugr/google_sheets/GwaQLFgqhfpjKNFb?tabId=Projects 2021");
-        const data = await res.json();
-        const project = data.data;
-        return project;
+
+        if (res.ok) {
+            const data = await res.json();
+            const project = data.data;
+            return project;
+        } else {
+            document.querySelector('li').style.display = 'none';
+            document.querySelector('.project-card').style.display = 'none';
+        }
+
     } catch (error) {
         console.error(error);
     }
 };
+//
