e9d038702ba2530f5b1eb472131fd9abec062daa
PanuGr <ppanagiotisilv@hotmail.com>
2021-10-02 21:19:46 +0300

    added 1 portfolio project

--- src/index.js
+++ src/index.js
@@ -6,16 +6,11 @@ import { getData } from './js/projectsAPI'; */
         getData().then((project) => { showProjects('#about article', project[0]); });
      */
 const buttonUp = document.querySelector("button");
-
-// showCourses();
-document.querySelector("button").addEventListener('click', () => window.scrollTo(0, 0))
+buttonUp.addEventListener('click', () => window.scrollTo(0, 0))
 window.addEventListener('scroll', function (e) {
     if (window.scrollY > 400) {
         buttonUp.style.visibility = "visible";
     } else { buttonUp.style.visibility = "hidden"; }
 })
+// showCourses();
 
-
-
-
-//buttonUp.onclick = () => window.scrollTo(0, 0);
