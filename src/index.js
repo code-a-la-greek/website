609a7be6aee9df333f03a6f35ec8dc20ffd2cbec
PanuGr <ppanagiotisilv@hotmail.com>
2021-11-06 10:44:18 +0200

    1 fetch request-latest design

--- src/index.js
+++ src/index.js
@@ -7,24 +7,19 @@ window.addEventListener('scroll', function (e) {
 })
 
 
-const cardImages = document.querySelectorAll("#challenges img");
-const cardTitle = document.querySelectorAll("#challenges h3");
-const cardText = document.querySelectorAll("#challenges p");
-const cardLiveDemo = document.querySelectorAll("#challenges .live");
-const cardGithub = document.querySelectorAll("#challenges .github");
+const cardImages = document.querySelector("#challenges section img");
+const cardTitle = document.querySelector("#challenges h3");
+const cardText = document.querySelector("#challenges section p");
+const cardLiveLink = document.querySelector("#challenges .live");
+const cardGithub = document.querySelector("#challenges .github");
 
 
 const getData = async () => {
     try {
         const latestDesignProject = "https://v1.nocodeapi.com/panugr/google_sheets/weAjdfuhKgnxoMqy?tabId=Projects&row_id=2";
-        const splitter = "https://v1.nocodeapi.com/panugr/google_sheets/weAjdfuhKgnxoMqy?tabId=Projects&row_id=3";
-        const weatherApp = "https://v1.nocodeapi.com/panugr/google_sheets/weAjdfuhKgnxoMqy?tabId=Projects&row_id=16";
-        const rockPaperScissors = "https://v1.nocodeapi.com/panugr/google_sheets/weAjdfuhKgnxoMqy?tabId=Projects&row_id=18";
-
-        const results = await Promise.all([fetch(latestDesignProject), fetch(splitter), fetch(weatherApp), fetch(rockPaperScissors)]);
-        const finalData = await Promise.all(results.map(result => result.json()));
-        // console.log(finalData);
-        return finalData;
+        const results = await fetch(latestDesignProject);
+        const data = await results.json();
+        return data;
     } catch (error) {
         console.error(error);
     }
@@ -32,27 +27,11 @@ const getData = async () => {
 
 getData().
     then(data => {
-        for (let i = 0; i < data.length; i++) {
-            const websiteLink = data[i].website;
-            const link = cardLiveDemo[i];
-            link.setAttribute("href", websiteLink);
-
-            const githubLink = data[i].github;
-            const gitLink = cardGithub[i];
-            gitLink.setAttribute("href", githubLink);
-
-           /*  const imageLink = data[i].image;
-            const imageSrc = cardImages[i];
-            imageSrc.setAttribute("src", imageLink);
-
-            const title = data[i].title;
-            const titleHtml = cardTitle[i];
-            titleHtml.innerHTML = title;
-
-            const text = data[i].info;
-            const textHtml = cardText[i];
-            textHtml.innerHTML = text; */
-        }
+        cardLiveLink.setAttribute("href", data.website);
+        cardGithub.setAttribute("href", data.github);
+        cardImages.setAttribute("src", data.image);
+        cardTitle.innerHTML = data.title;
+        cardText.innerHTML = data.info;
     });
- 
+
 
