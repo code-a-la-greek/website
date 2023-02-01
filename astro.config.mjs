bc2688f5115a27041761cfdabbed1701fc8c4482
PanuGr <ppanagiotisilv@hotmail.com>
2023-02-01 21:39:38 +0200

    netlify_cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import NetlifyCMS from 'astro-netlify-cms';
 
 export default defineConfig({
   markdown: {
@@ -10,5 +11,34 @@ export default defineConfig({
     },
   },
   site: 'https://panagiotis.netlify.app',
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    NetlifyCMS({
+      config: {
+        publish_mode: 'editorial_workflow',
+        backend: {
+          name: 'git-gateway',
+          branch: 'main'
+        },
+        collections: [
+          /*content collections*/
+          // Define a blog post collection
+          {
+            name: 'posts',
+            label: 'My projects',
+            folder: 'src/pages/post',
+            format: 'frontmatter',
+            create: true,
+            delete: true,
+            fields: [
+              { name: 'image', widget: 'image', label: 'Post Image', media_folder:'/images/', allow_multiple:'false' },
+              { name: 'title', widget: 'string', label: 'Post Title' },
+              { name: 'body', widget: 'markdown', label: 'Post Body' },
+            ],
+          },
+        ],
+      },
+    }),
+  ],
 });
