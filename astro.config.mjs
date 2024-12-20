295cb2f0ba6158f8d84083827eb6295074f82ebe
ppanagiotisilv <ppanagiotisilv@gmail.com>
2024-12-20 21:57:48 +0000

    remove netlify cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import NetlifyCMS from 'astro-netlify-cms';
 
 export default defineConfig({
   markdown: {
@@ -11,45 +10,5 @@ export default defineConfig({
     },
   },
   site: 'https://codealagreek.netlify.app',
-  integrations: [
-    tailwind(),
-    sitemap(),
-    NetlifyCMS({
-      config: {
-        publish_mode: 'editorial_workflow',
-        //logo_url: 'https://source.unsplash.com/weekly',
-        media_folder:'src/assets/images',
-        public_folder:'/assets/images',
-        backend: {
-          name: 'git-gateway',
-          branch: 'master'
-        },
-        collections: [
-          {
-            name: 'posts',
-            label: 'My projects',
-            folder: 'src/pages/post',
-            format: 'frontmatter',
-            create: true,
-            delete: true,
-            fields: [
-              { label: "Layout", name: "layout", widget: "hidden", default: "../../layouts/PostLayout.astro" },
-              { name: 'image', widget: 'image', label: 'Post Image' },
-              { name: 'categories', widget: 'select', label: 'Post category', multiple:true, min: 1, options: ['challenges', 'newbie', 'junior', 'intermediate', 'advance', 'guru', 'portfolio', 'css','javascript','blog','html'] },
-              {
-                name: 'date',//frontmatter format
-                widget: 'datetime',
-                format: 'YYYY-MM-DD',// frontmatter format
-                date_format: 'DD MMM YYYY',//ετσι φαινεται στο UI
-                label: 'Publish Date',
-                time_format:false
-              },
-              { name: 'title', widget: 'string', label: 'Post Title' },
-              { name: 'body', widget: 'markdown', label: 'Post Body' },
-            ],
-          },
-        ],
-      },
-    }),
-  ],
+  integrations: [tailwind(),sitemap()],
 });
